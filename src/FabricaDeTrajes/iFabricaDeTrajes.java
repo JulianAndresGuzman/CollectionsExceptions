43e764008f4677a71d6ee09d4ec92526f4a1480f
Julian Andres Guzman Cuellar <julianguzmn28@gmail.com>
2024-05-18 17:35:35 -0500

    Interfaz ifabrica de trajes

---
+++ src/FabricaDeTrajes/iFabricaDeTrajes.java
@@ -0,0 +1,12 @@
+
+package FabricaDeTrajes;
+
+public interface iFabricaDeTrajes {
+    
+  void añadirComponenteAlmacen();
+  void ListarComponentes();
+  void añadirTrajeAlmacen();
+  void ListarTrajes();
+  void activarDesactivarRebajas();
+  void crearEnvío();
+}
