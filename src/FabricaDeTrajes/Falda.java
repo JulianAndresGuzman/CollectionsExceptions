8db917660e4a2bc348b35e79924d20ca5d7844aa
Julian Andres Guzman Cuellar <julianguzmn28@gmail.com>
2024-05-18 17:21:43 -0500

    Clase falda

---
+++ src/FabricaDeTrajes/Falda.java
@@ -0,0 +1,27 @@
+package FabricaDeTrajes;
+
+/**
+ *
+ * @author JulianAndresGuzmanCuellar 20232215518
+ */
+public class Falda {
+
+    private boolean conCremallera;
+
+    public Falda(boolean conCremallera) {
+        this.conCremallera = conCremallera;
+    }
+
+    public boolean isConCremallera() {
+        return conCremallera;
+    }
+
+    public void setConCremallera(boolean conCremallera) {
+        this.conCremallera = conCremallera;
+    }
+
+    @Override
+    public String toString() {
+        return "Tiene cremallera: " + conCremallera;
+    }
+}
