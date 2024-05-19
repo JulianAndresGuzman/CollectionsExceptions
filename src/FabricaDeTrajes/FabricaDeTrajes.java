c67610e0f5d07b722e09ec8833bbd840656ee307
Julian Andres Guzman Cuellar <julianguzmn28@gmail.com>
2024-05-18 22:37:51 -0500

    Metodo EscribirMenu

--- src/FabricaDeTrajes/FabricaDeTrajes.java
+++ src/FabricaDeTrajes/FabricaDeTrajes.java
@@ -1,11 +1,10 @@
-
 package FabricaDeTrajes;
 
 import java.util.ArrayList;
 import java.util.TreeSet;
 
- class FabricaDeTrajes implements iFabricaDeTrajes {
-    
+class FabricaDeTrajes implements iFabricaDeTrajes {
+
     private ArrayList<Componente> componentesEnAlmacen;
     private TreeSet<Traje> trajesEnAlmacen;
     private boolean sonRebajas;
@@ -15,9 +14,21 @@ import java.util.TreeSet;
         this.trajesEnAlmacen = new TreeSet<>();
         this.sonRebajas = false;
     }
-    
-    public void EscribirMenu(){
+
+    public void EscribirMenu() {
         
+        System.out.println("***************************************");
+        System.out.println("MENU FABRICA DE TRAJES");
+        System.out.println("1.- Añadir Componente a almacén");
+        System.out.println("2.- Listar Componentes del almacén");
+        System.out.println("3.- Crear traje y añadir a almacén");
+        System.out.println("4.- Listar trajes del almacén");
+        System.out.println("5.- Activar/Desactivar las rebajas");
+        System.out.println("6.- Crear envío");
+        System.out.println("7.- Crea componentes de prueba");
+        System.out.println("0.- Salir");
+        System.out.println("****************************************");
+
     }
 
     @Override
@@ -49,10 +60,5 @@ import java.util.TreeSet;
     public void crearEnvío() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-  
+
 }
-
-   
-   
-    
-
